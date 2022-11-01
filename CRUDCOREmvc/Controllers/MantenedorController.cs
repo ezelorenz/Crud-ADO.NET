c32dae7cd2dc2d438e772ab8fba5a42fc877e540
Ezequiel Lorenz <eze_lorenz@hotmail.com>
2022-11-01 12:41:07 -0300

    subida archivos

---
+++ CRUDCOREmvc/Controllers/MantenedorController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CRUDCOREmvc.Datos;
+using CRUDCOREmvc.Models;
+
+namespace CRUDCOREmvc.Controllers
+{
+    public class MantenedorController : Controller
+    {
+        ContactoDatos _ContactoDatos = new ContactoDatos();
+        public IActionResult Listar()
+        {
+            //LA VISTA MOSTRARA UNA LISTA DE CONTACTOS
+            var oLista = _ContactoDatos.Listar();
+            return View(oLista);
+        }
+        public IActionResult Guardar()
+        {
+            //METODO QUE SOLO DEVUELVE LA VISTA
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Guardar(ContactoModel oContacto)
+        {
+            //METODO QUE RECIBE EL OBJETO PARA GUARDARLO EN LE BASE DE DATOS
+
+            if (!ModelState.IsValid)
+                return View();
+
+            bool respuesta = _ContactoDatos.Guardar(oContacto);
+            if (respuesta)
+                return RedirectToAction("Listar");
+            else
+            return View();
+        }
+        public IActionResult Editar(int IdContacto)
+        {
+            var oContacto = _ContactoDatos.Obtener(IdContacto);
+            //METODO QUE SOLO DEVUELVE LA VISTA
+            return View(oContacto);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(ContactoModel oContacto)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            bool respuesta = _ContactoDatos.Editar(oContacto);
+            if (respuesta)
+                return RedirectToAction("Listar");
+            else
+                return View();
+        }
+        public IActionResult Eliminar(int IdContacto)
+        {
+            var oContacto = _ContactoDatos.Obtener(IdContacto);
+            //METODO QUE SOLO DEVUELVE LA VISTA
+            return View(oContacto);
+        }
+
+        [HttpPost]
+        public IActionResult Eliminar(ContactoModel oContacto)
+        {
+            bool respuesta = _ContactoDatos.Eliminar(oContacto.IdContacto);
+            if (respuesta)
+                return RedirectToAction("Listar");
+            else
+                return View();
+        }
+    }
+}
---
+++ CRUDCOREmvc/Models/ContactoModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUDCOREmvc.Models
+{
+    public class ContactoModel
+    {
+        //cada una de éstas lineas hacen referencia a cada una de las columnas que tengo en la tabla
+        public int IdContacto { get; set; }
+        [Required(ErrorMessage ="El campo Nombre es obligatorio")]
+        public string? Nombre { get; set; }
+        [Required(ErrorMessage = "El campo Telefono es obligatorio")]
+        public string? Telefono { get; set; }
+        [Required(ErrorMessage = "El campo Correo es obligatorio")]
+        public string? Correo { get; set; }
+    }
+}
